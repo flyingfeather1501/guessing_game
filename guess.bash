cb411a7f01356e84d75524c186bd889b35aad60c
Moonlight Feather <flyingfeather1501@gmail.com>
2017-03-15 22:57:07 +0800

    Fix the game was never progressing

--- guess.bash
+++ guess.bash
@@ -4,20 +4,24 @@
 # Generate a random number. The player guesses, then we tell them if it's too low. too high, or correct.
 # Upon receiving the correct input, exit.
 
+generate_number () {
+  export answer=$(((RANDOM%100)+1))
+}
+
 # random number
-answer=$(((RANDOM%100)+1))
+generate_number
 guess=""
 
 while true; do
   read -p "Your guess is (integer, 1-100): " guess
 
   # Check if $guess is a number 
-  [[ "$((guess))" != "$guess" ]] && echo "Numbers please!"; continue
+  [[ "$((guess))" != "$guess" ]] && echo "Numbers please!" && continue
 
   if [[ "$guess" == "$answer" ]]; then
-    read -p "Correct! Play again (y/*)? " yn
-    case yn in
-      y*)continue;;
+    read -p "Correct! Play again (y/n)? " yn
+    case $yn in
+      y*)generate_number; continue;;
       *)exit;;
     esac
   fi
